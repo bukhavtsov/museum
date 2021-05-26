d0aa3cc2a21f4d73068dc769e35f96b0c07608c9
Artsiom Bukhautsou <artyom.bukhavtsov@gmail.com>
2021-05-26 20:10:20 +0300

    refactored artifacts elements create and started integration with add artifact

--- front-end/src/app/artifacts/admin/add-form/artifacts-elements-create/artifacts-elements-create.component.ts
+++ front-end/src/app/artifacts/admin/add-form/artifacts-elements-create/artifacts-elements-create.component.ts
@@ -1,4 +1,3 @@
-import {SelectionModel} from '@angular/cdk/collections';
 import {FlatTreeControl} from '@angular/cdk/tree';
 import {Component, Injectable} from '@angular/core';
 import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
@@ -6,27 +5,16 @@ import {BehaviorSubject} from 'rxjs';
 import {Artifact, ArtifactElement, ArtifactService} from '../../../../shared/artifactService';
 import {Router} from '@angular/router';
 
-/** Flat to-do name node with expandable and level information */
-export class TodoItemFlatNode {
+/** Flat Artifact Element name node with expandable and level information */
+export class ArtifactElementFlatNode {
     name: string;
     level: number;
     expandable: boolean;
 }
 
-/**
- * The Json object for to-do list data.
- */
-const TREE_DATA = {
-        children: [{name: ''}],
-    };
 
-/**
- * Checklist database, it can build a tree structured Json object.
- * Each node in Json object represents a to-do name or a category.
- * If a node is a category, it has children items and new items can be added under the category.
- */
 @Injectable()
-export class ChecklistDatabase {
+export class ArtifactElementsDatasource {
     dataChange = new BehaviorSubject<ArtifactElement[]>([]);
 
     get data(): ArtifactElement[] {
@@ -38,88 +26,49 @@ export class ChecklistDatabase {
     }
 
     initialize() {
-        // Build the tree nodes from Json object. The result is a list of `ArtifactElement` with nested
-        //     file node as children.
-        const data = [{name:''}]
-
-        // Notify the change.
+        const data = [{name: ''}]
         this.dataChange.next(data);
     }
 
-    /**
-     * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
-     * The return value is the list of `ArtifactElement`.
-     */
-    buildFileTree(obj: { [key: string]: any }, level: number): ArtifactElement[] {
-        return Object.keys(obj).reduce<ArtifactElement[]>((accumulator, key) => {
-            const value = obj[key];
-            const node: ArtifactElement = {
-                name: '',
-                children: []
-            }
-            node.name = key;
-
-            if (value != null) {
-                if (typeof value === 'object') {
-                    node.children = this.buildFileTree(value, level + 1);
-                } else {
-                    node.name = value;
-                }
-            }
-
-            return accumulator.concat(node);
-        }, []);
-    }
-
-    /** Add an name to to-do list */
-    insertItem(parent: ArtifactElement, name: string) {
+    insertElement(parent: ArtifactElement, name: string) {
         if (parent.children) {
             parent.children.push({name: name} as ArtifactElement);
             this.dataChange.next(this.data);
         }
     }
 
-    updateItem(node: ArtifactElement, name: string) {
+    updateElement(node: ArtifactElement, name: string) {
         node.name = name;
         this.dataChange.next(this.data);
     }
 }
 
-/**
- * @title Tree with checkboxes
- */
 @Component({
     selector: 'app-artifacts-elements-create',
     templateUrl: `artifacts-elements-create.component.html`,
     styleUrls: ['artifacts-elements-create.component.scss'],
-    providers: [ChecklistDatabase]
+    providers: [ArtifactElementsDatasource]
 })
 export class ArtifactsElementsCreateComponent {
     /** Map from flat node to nested node. This helps us finding the nested node to be modified */
-    flatNodeMap = new Map<TodoItemFlatNode, ArtifactElement>();
+    flatNodeMap = new Map<ArtifactElementFlatNode, ArtifactElement>();
 
     /** Map from nested node to flattened node. This helps us to keep the same object for selection */
-    nestedNodeMap = new Map<ArtifactElement, TodoItemFlatNode>();
-
-    /** A selected parent node to be inserted */
-    selectedParent: TodoItemFlatNode | null = null;
+    nestedNodeMap = new Map<ArtifactElement, ArtifactElementFlatNode>();
 
     /** The new name's name */
-    newItemName = '';
+    newElementName = '';
 
-    treeControl: FlatTreeControl<TodoItemFlatNode>;
+    treeControl: FlatTreeControl<ArtifactElementFlatNode>;
 
-    treeFlattener: MatTreeFlattener<ArtifactElement, TodoItemFlatNode>;
+    treeFlattener: MatTreeFlattener<ArtifactElement, ArtifactElementFlatNode>;
 
-    dataSource: MatTreeFlatDataSource<ArtifactElement, TodoItemFlatNode>;
+    dataSource: MatTreeFlatDataSource<ArtifactElement, ArtifactElementFlatNode>;
 
-    /** The selection for checklist */
-    checklistSelection = new SelectionModel<TodoItemFlatNode>(true /* multiple */);
-
-    constructor(private _database: ChecklistDatabase, private _artifactService: ArtifactService, private _router: Router) {
+    constructor(private _database: ArtifactElementsDatasource, private _artifactService: ArtifactService, private _router: Router) {
         this.treeFlattener = new MatTreeFlattener(this.transformer, this.getLevel,
-            this.isExpandable, this.getChildren);
-        this.treeControl = new FlatTreeControl<TodoItemFlatNode>(this.getLevel, this.isExpandable);
+            this.alwaysExpandable, this.getChildren);
+        this.treeControl = new FlatTreeControl<ArtifactElementFlatNode>(this.getLevel, this.alwaysExpandable);
         this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
         _database.dataChange.subscribe(data => {
@@ -127,17 +76,15 @@ export class ArtifactsElementsCreateComponent {
         });
     }
 
-    getLevel = (node: TodoItemFlatNode) => node.level;
+    getLevel = (node: ArtifactElementFlatNode) => node.level;
 
-    isExpandable = (node: TodoItemFlatNode) => true;
+    alwaysExpandable = (node: ArtifactElementFlatNode) => true;
 
     getChildren = (node: ArtifactElement): ArtifactElement[] => node.children;
 
-    hasChild = (_: number, _nodeData: TodoItemFlatNode) => true;
+    alwaysAllowCreate = (_: number, _nodeData: ArtifactElementFlatNode) => true;
 
-    // hasChild = (_: number, _nodeData: TodoItemFlatNode) => _nodeData.expandable;
-
-    hasNoContent = (_: number, _nodeData: TodoItemFlatNode) => _nodeData.name === '';
+    hasNoContent = (_: number, _nodeData: ArtifactElementFlatNode) => _nodeData.name === '';
 
     /**
      * Transformer to convert nested node to flat node. Record the nodes in maps for later use.
@@ -146,7 +93,7 @@ export class ArtifactsElementsCreateComponent {
         const existingNode = this.nestedNodeMap.get(node);
         const flatNode = existingNode && existingNode.name === node.name
             ? existingNode
-            : new TodoItemFlatNode();
+            : new ArtifactElementFlatNode();
         flatNode.name = node.name;
         flatNode.level = level;
         flatNode.expandable = !!node.children?.length;
@@ -155,66 +102,8 @@ export class ArtifactsElementsCreateComponent {
         return flatNode;
     }
 
-    /** Whether all the descendants of the node are selected. */
-    descendantsAllSelected(node: TodoItemFlatNode): boolean {
-        const descendants = this.treeControl.getDescendants(node);
-        const descAllSelected = descendants.length > 0 && descendants.every(child => {
-            return this.checklistSelection.isSelected(child);
-        });
-        return descAllSelected;
-    }
-
-    /** Whether part of the descendants are selected */
-    descendantsPartiallySelected(node: TodoItemFlatNode): boolean {
-        const descendants = this.treeControl.getDescendants(node);
-        const result = descendants.some(child => this.checklistSelection.isSelected(child));
-        return result && !this.descendantsAllSelected(node);
-    }
-
-    /** Toggle the to-do name selection. Select/deselect all the descendants node */
-    todoItemSelectionToggle(node: TodoItemFlatNode): void {
-        this.checklistSelection.toggle(node);
-        const descendants = this.treeControl.getDescendants(node);
-        this.checklistSelection.isSelected(node)
-            ? this.checklistSelection.select(...descendants)
-            : this.checklistSelection.deselect(...descendants);
-
-        // Force update for the parent
-        descendants.forEach(child => this.checklistSelection.isSelected(child));
-        this.checkAllParentsSelection(node);
-    }
-
-    /** Toggle a leaf to-do name selection. Check all the parents to see if they changed */
-    todoLeafItemSelectionToggle(node: TodoItemFlatNode): void {
-        this.checklistSelection.toggle(node);
-        this.checkAllParentsSelection(node);
-    }
-
-    /* Checks all the parents when a leaf node is selected/unselected */
-    checkAllParentsSelection(node: TodoItemFlatNode): void {
-        let parent: TodoItemFlatNode | null = this.getParentNode(node);
-        while (parent) {
-            this.checkRootNodeSelection(parent);
-            parent = this.getParentNode(parent);
-        }
-    }
-
-    /** Check root node checked state and change it accordingly */
-    checkRootNodeSelection(node: TodoItemFlatNode): void {
-        const nodeSelected = this.checklistSelection.isSelected(node);
-        const descendants = this.treeControl.getDescendants(node);
-        const descAllSelected = descendants.length > 0 && descendants.every(child => {
-            return this.checklistSelection.isSelected(child);
-        });
-        if (nodeSelected && !descAllSelected) {
-            this.checklistSelection.deselect(node);
-        } else if (!nodeSelected && descAllSelected) {
-            this.checklistSelection.select(node);
-        }
-    }
-
     /* Get the parent node of a node */
-    getParentNode(node: TodoItemFlatNode): TodoItemFlatNode | null {
+    getParentNode(node: ArtifactElementFlatNode): ArtifactElementFlatNode | null {
         const currentLevel = this.getLevel(node);
 
         if (currentLevel < 1) {
@@ -234,40 +123,18 @@ export class ArtifactsElementsCreateComponent {
     }
 
     /** Select the category so we can insert the new name. */
-    addNewItem(node: TodoItemFlatNode) {
-        console.log(node)
-        if (!node.expandable) {
-            node.expandable = true
-        }
+    addNewElement(node: ArtifactElementFlatNode) {
         const parentNode = this.flatNodeMap.get(node);
-        console.log('parent node', parentNode)
         if (parentNode.children === undefined) {
             parentNode.children = []
         }
-        this._database.insertItem(parentNode!, '');
+        this._database.insertElement(parentNode!, '');
         this.treeControl.expand(node);
     }
 
     /** Save the node to database */
-    saveNode(node: TodoItemFlatNode, itemValue: string) {
+    saveNode(node: ArtifactElementFlatNode, elementValue: string) {
         const nestedNode = this.flatNodeMap.get(node);
-        this._database.updateItem(nestedNode!, itemValue);
-    }
-
-    public createArtifact() {
-        const newArtifact: Artifact = {
-            creator: 'test',
-            artifact_style: 'test',
-            date_exc: '2021-05-14',
-            transferred_by: 'test',
-            artifact_measurement: {
-                length: 10,
-                width: 20,
-                height: 30
-            },
-            artifact_elements: this.dataSource._data.getValue()
-        };
-        this._artifactService.add(newArtifact);
-        this._router.navigate(['/artifacts'])
+        this._database.updateElement(nestedNode!, elementValue);
     }
 }
